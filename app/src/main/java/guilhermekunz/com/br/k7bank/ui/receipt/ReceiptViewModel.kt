537c835da4a00e856de73deed8200ba9076bf615
guilherme-kunz <guilhermebutzenkunz@gmail.com>
2022-04-22 12:41:08 -0300

    ReceiptViewModel.kt

--- app/src/main/java/guilhermekunz/com/br/k7bank/ui/receipt/ReceiptViewModel.kt
+++ app/src/main/java/guilhermekunz/com/br/k7bank/ui/receipt/ReceiptViewModel.kt
@@ -1,8 +1,26 @@
 package guilhermekunz.com.br.k7bank.ui.receipt
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import guilhermekunz.com.br.k7bank.api.response.DetailStatementResponse
 import guilhermekunz.com.br.k7bank.repository.Repository
+import kotlinx.coroutines.launch
 
 class ReceiptViewModel(val repository: Repository) : ViewModel() {
+
+    private val _statementDetailResponse = MutableLiveData<DetailStatementResponse>()
+    val statementDetailResponse = _statementDetailResponse as LiveData<DetailStatementResponse>
+
+    fun statementDetail(id: String) = viewModelScope.launch {
+        try {
+            val response = repository.getMyStatementDetail(id)
+            _statementDetailResponse.value = response!!
+        } catch (e: Throwable) {
+            Log.e("Data", e.message.toString())
+        }
+    }
 
 }
